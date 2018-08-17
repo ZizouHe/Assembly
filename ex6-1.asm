3dc44a179bc70ccca7b4dada38762f75888a40f9
ZizouHe <lihezizou@gmail.com>
2018-08-17 19:39:04 +0100

    finish chapter 7

---
+++ ex6-1.asm
@@ -0,0 +1,50 @@
+assume cs:codesg,ss:stacksg,ds:datasg
+
+stacksg segment
+
+    dw 0,0,0,0,0,0,0,0
+
+stacksg ends
+
+datasg segment
+
+    db '1. display      '
+    db '2. brows        '
+    db '3. replace      '
+    db '4. modify       '
+
+datasg ends
+
+codesg segment
+
+start:
+
+    mov ax, stacksg
+    mov ss, ax
+    mov sp, 10H
+    mov ax, datasg
+    mov ds, ax
+    mov bx, 0
+    mov cx, 4
+
+s:  push cx
+    mov cx, 4
+    mov si, 3
+
+s1: mov al, [si+bx]
+    and al, 11011111B
+    mov [si+bx], al
+    inc si
+    loop s1
+
+    pop cx
+    add bx, 16
+    loop s
+
+    mov ax, 4c00h
+    int 21h
+
+codesg ends
+
+end start
+
